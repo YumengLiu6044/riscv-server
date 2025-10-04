bfbdd7f2e8422b10e678d1e2d6a1eca574801807
Yumeng Liu <lym20041026@gmail.com>
2025-10-04 11:12:54 -0700

    setup script

---
+++ server_init.sh
@@ -0,0 +1,26 @@
+sudo rm -rf /opt/riscv
+
+sudo apt update
+sudo apt install autoconf automake python3 libmpc-dev libmpfr-dev \
+    libgmp-dev gawk bison flex texinfo patchutils gcc g++ zlib1g-dev \
+    libexpat1-dev libslirp-dev git
+
+sudo mkdir /opt/riscv
+sudo chown $(whoami):$(whoami) /opt/riscv
+
+git clone https://github.com/riscv-collab/riscv-gnu-toolchain
+cd riscv-gnu-toolchain
+make clean
+./configure --prefix=/opt/riscv \
+    --with-arch=rv64g --with-abi=lp64 \
+    --enable-qemu-system
+make linux
+
+echo 'export PATH=$PATH:/opt/riscv/bin' >> ~/.bashrc
+echo 'export QEMU_LD_PREFIX=/opt/riscv/sysroot' >> ~/.bashrc
+source ~/.bashrc
+
+for tool in ar as gcc gdb ld nm objdump readelf strip; do
+    sudo ln -sf /opt/riscv/bin/riscv64-unknown-linux-gnu-$tool /opt/riscv/bin/rv-$tool
+done
+
