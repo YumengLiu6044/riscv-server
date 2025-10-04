7a4950b31d76da1251d769dd64f955b840fe8ca3
Yumeng Liu <lym20041026@gmail.com>
2025-10-04 11:20:34 -0700

    added QEMU

--- server_init.sh
+++ server_init.sh
@@ -1,10 +1,13 @@
+# Clean up existing data
 sudo rm -rf /opt/riscv
 
+# Install dependencies
 sudo apt update
 sudo apt install autoconf automake python3 libmpc-dev libmpfr-dev \
     libgmp-dev gawk bison flex texinfo patchutils gcc g++ zlib1g-dev \
     libexpat1-dev libslirp-dev git
 
+# Build riscv toolchain from source
 sudo mkdir /opt/riscv
 sudo chown $(whoami):$(whoami) /opt/riscv
 
@@ -16,6 +19,11 @@ make clean
     --enable-qemu-system
 make linux
 
+# Install QEMU
+sudo apt install qemu-user qemu-user-binfmt qemu-system-misc
+sudo systemctl restart systemd-binfmt
+
+# Add tools to path
 echo 'export PATH=$PATH:/opt/riscv/bin' >> ~/.bashrc
 echo 'export QEMU_LD_PREFIX=/opt/riscv/sysroot' >> ~/.bashrc
 source ~/.bashrc
